c8e7c195e4dbf13e29a86ec4cba74d78e400fc54
OVAWARE <116229855+OVAWARE@users.noreply.github.com>
2022-12-06 18:29:43 -0500

    Update Flight.lua

--- Flight.lua
+++ Flight.lua
@@ -1,1 +1,17 @@
+local radar = peripheral.wrap("left")
+local players = radar.getPlayers()
 
+local playerName = ""
+local distance = 0
+
+for i = 1, #players do
+    for k, v in pairs(players) do
+        if players[i]["name"] ~= "" or players[i]["name"] ~= nil then
+            playerName = players[i]["name"]
+        end
+        if players[i]["distance"] ~= nil then
+            distance = players[i]["distance"]
+        end
+        print("Player: " .. playerName .. " -- Distance: " .. distance)
+    end
+end

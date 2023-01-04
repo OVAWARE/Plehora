c8acc0421bd6ef62818661046bc96853b0198b74
OVAWARE <116229855+OVAWARE@users.noreply.github.com>
2023-01-03 21:35:37 -0500

    Update Flight.lua

--- Flight.lua
+++ Flight.lua
@@ -1,17 +1,46 @@
-local radar = peripheral.wrap("left")
-local players = radar.getPlayers()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
 
-local playerName = ""
-local distance = 0
+// Function to generate a UUID
+char* generate_uuid() {
+  char* uuid = malloc(37);
+  snprintf(uuid, 37, "%04x%04x-%04x-%04x-%04x-%04x%04x%04x",
+    rand() & 0xffff, rand() & 0xffff,
+    rand() & 0xffff,
+    rand() & 0xffff | 0x4000,
+    rand() & 0xffff | 0x8000,
+    rand() & 0xffff, rand() & 0xffff, rand() & 0xffff);
+  return uuid;
+}
 
-for i = 1, #players do
-    for k, v in pairs(players) do
-        if players[i]["name"] ~= "" or players[i]["name"] ~= nil then
-            playerName = players[i]["name"]
-        end
-        if players[i]["distance"] ~= nil then
-            distance = players[i]["distance"]
-        end
-        print("Player: " .. playerName .. " -- Distance: " .. distance)
-    end
-end
+// Function to send a request to the create API with the UUID as the USERNAME parameter
+void send_request(char* uuid) {
+  // Initialize the curl library
+  CURL* curl = curl_easy_init();
+  if (curl) {
+    // Format the URL of the create API with the UUID as the USERNAME parameter
+    char url[100];
+    sprintf(url, "http://localhost:8080/CreateAgent.php?USERNAME=%s", uuid);
+
+    // Set the URL of the create API
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+
+    // Send the request
+    curl_easy_perform(curl);
+
+    // Clean up the curl library
+    curl_easy_cleanup(curl);
+  }
+}
+
+int main(int argc, char** argv) {
+  // Generate a UUID
+  char* uuid = generate_uuid();
+
+  // Send a request to the create API with the UUID as the USERNAME parameter
+  send_request(uuid);
+
+  return 0;
+}
